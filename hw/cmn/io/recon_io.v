78d90487ebc9ad9497465af757ff6882f4abda35
Jeff Lieu <lieumychuong@gmail.com>
2017-05-19 23:03:24 +1000

    initial commit of basic files

---
+++ hw/cmn/io/recon_io.v
@@ -0,0 +1,240 @@
+/*
+  Project: RECON 2017
+  Author: Jeff Lieu <lieumychuong@gmail.com>
+  Description: IO port that supports up to 32 pins 
+                + Support PWM
+                + Support Interrupts on Changes of Input 
+
+*/
+
+/////////////////////////////////////////////////////////////////////////////////
+// synthesis translate_off
+`timescale 1ns / 1ps
+// synthesis translate_on
+
+// turn off superfluous verilog processor warnings 
+// altera message_level Level1 
+// altera message_off 10034 10035 10036 10037 10230 10240 10030 
+
+/* Address definition */
+`define     DIR_OFFSET    0
+`define     OUT_OFFSET    1
+`define      IN_OFFSET    2
+`define OUT_SET_OFFSET    3
+`define OUT_CLR_OFFSET    4
+`define OPENDRN_OFFSET    5 
+`define PWM_ENA_OFFSET    6
+`define IRQ_STATUS_OFFSET 7
+`define IRQ_ENA_OFFSET    8
+`define IRQ_REDGE_OFFSET  9
+`define IRQ_FEDGE_OFFSET 10
+`define DBNC_ENA_OFFSET  11
+`define PWM_VALUE_OFFSET 16
+
+
+module recon_io (
+                       // Avalon Interface
+                       address,
+                       chipselect,
+                       clk,
+                       reset_n,
+                       write,
+                       read,
+                       writedata,
+                       readdata,
+                       
+                       irq,
+                       
+                       // IO Port
+                       io_out   ,
+                       io_in    ,
+                       io_oe    ,
+                       io_opdrn
+                       
+                    )
+;
+  parameter        PORT_WIDTH = 16;
+  parameter        PWM_CNTR_WIDTH  = 8;  
+  output  [ 31: 0] readdata;
+  input   [  5: 0] address;
+  input            chipselect;
+  input            clk;
+  input            reset_n;
+  input            write;
+  input            read;
+  input   [ 31: 0] writedata;
+  output  [PORT_WIDTH-1:0] io_out;
+  output  [PORT_WIDTH-1:0] io_oe;
+  output  [PORT_WIDTH-1:0] io_opdrn;
+  input   [PORT_WIDTH-1:0] io_in;
+  output  irq;
+  
+  wire    [ 31: 0] readdata;
+  wire             clk_en;
+  reg     [ PORT_WIDTH-1: 0] data_out;
+  wire    [ PORT_WIDTH-1: 0] out_port;
+  reg     [ PORT_WIDTH-1: 0] read_mux_reg;
+  
+  reg     [ PORT_WIDTH-1: 0] pin_mode;
+  reg     [ PORT_WIDTH-1: 0] pin_opdrn;
+  reg     [ PORT_WIDTH-1: 0] data_in;
+  reg     [ PORT_WIDTH-1: 0] data_in_r1;
+  reg     [ PORT_WIDTH-1: 0] data_in_r2;
+  reg     [ PORT_WIDTH-1: 0] data_in_event;  
+  reg     [ PORT_WIDTH-1: 0] pwm_ena;
+  reg     [ PORT_WIDTH-1: 0] pwm_out;
+  reg     [ PORT_WIDTH-1: 0] irq_ena;
+  reg     [ PORT_WIDTH-1: 0] irq_redge;
+  reg     [ PORT_WIDTH-1: 0] irq_fedge;
+  reg     [ PORT_WIDTH-1: 0] irq_status;
+  reg     [ PWM_CNTR_WIDTH-1: 0] pwm_values[PORT_WIDTH-1:0];
+  reg     [ PWM_CNTR_WIDTH-1: 0] pwm_cntr;
+  
+  
+  assign clk_en = 1;
+ 
+  initial 
+    begin 
+      pwm_ena <= 0;
+    end 
+  
+  
+  always@(posedge clk)
+  begin 
+    if (chipselect && read)
+      case(address)
+      `DIR_OFFSET       : read_mux_reg <= pin_mode;
+      `OUT_OFFSET       : read_mux_reg <= data_out;
+      `IN_OFFSET        : read_mux_reg <= data_in;
+      `OUT_SET_OFFSET   : read_mux_reg <= 32'h0;
+      `OUT_CLR_OFFSET   : read_mux_reg <= 32'h0;
+      `OPENDRN_OFFSET   : read_mux_reg <= pin_opdrn;
+      `PWM_ENA_OFFSET   : read_mux_reg <= pwm_ena;
+      `IRQ_STATUS_OFFSET: read_mux_reg <= irq_status;
+      `IRQ_ENA_OFFSET   : read_mux_reg <= irq_ena;
+      `IRQ_REDGE_OFFSET : read_mux_reg <= irq_redge;
+      `IRQ_FEDGE_OFFSET : read_mux_reg <= irq_fedge;
+      default           : read_mux_reg <= data_in;
+      endcase 
+    end 
+  assign readdata = {32'b0 | read_mux_reg};
+  
+  /* Settings registers */
+  integer I;  
+  always @(posedge clk or negedge reset_n)
+    begin
+      if (reset_n == 0) begin 
+          pin_mode <= 0;
+          pwm_ena  <= 0;
+          irq_ena  <= 0;
+          irq_redge<= 0;
+          irq_fedge<= 0;        
+        end 
+      else begin 
+        if (chipselect && write && (address == `DIR_OFFSET)) begin 
+          pin_mode  <= writedata;
+        end 
+        if (chipselect && write && (address == `PWM_ENA_OFFSET)) begin 
+          pwm_ena   <= writedata;
+        end
+        if (chipselect && write && (address == `IRQ_ENA_OFFSET)) begin 
+          irq_ena   <= writedata;
+        end
+        if (chipselect && write && (address == `IRQ_REDGE_OFFSET)) begin 
+          irq_redge <= writedata;
+        end
+        if (chipselect && write && (address == `IRQ_FEDGE_OFFSET)) begin 
+          irq_fedge <= writedata;
+        end
+      end       
+    end
+  
+  /* 
+  Data Out Register 
+  */
+  always @(posedge clk or negedge reset_n)
+    begin
+      if (reset_n == 0)
+          data_out <= 0;          
+      else 
+        for(I=0;I<PORT_WIDTH;I=I+1) 
+        begin 
+          if (chipselect && write && (address == `OUT_OFFSET))
+              data_out[I] <= writedata[I];
+          else if (chipselect && write && (address == `OUT_SET_OFFSET))
+            begin 
+              if (writedata[I]==1'b1) data_out[I] <= 1'b1;        
+            end 
+          else if (chipselect && write && (address == `OUT_CLR_OFFSET))
+            begin 
+              if (writedata[I]==1'b1) data_out[I] <= 1'b0;
+            end 
+        end    
+    end
+  
+  /* 
+    Data In is read directly 
+  */  
+  always@(posedge clk)
+    begin 
+      data_in <= io_in;
+      data_in_r1 <= data_in;
+      data_in_r2 <= data_in_r1;
+      
+      /* Detect rising edge and falling edge if it's enabled */
+      data_in_event <= (irq_redge & (data_in_r1 & ~data_in_r2))|
+                       (irq_fedge & (~data_in_r1 & data_in_r2));
+      
+      /* 
+         Latch the event, if the IRQ is enabled for this pin 
+         or clear when we write 1 to the status
+      */
+      for(I=0;I<PORT_WIDTH;I=I+1)
+        if(data_in_event[I]==1'b1 && irq_ena[I]==1'b1)
+          irq_status[I] <= 1'b1;
+        else if (chipselect && write && address == `IRQ_STATUS_OFFSET && writedata[I]==1'b1)
+          irq_status[I] <= 1'b0;
+    end 
+  assign irq = | irq_status;
+  
+  /* 
+  PWM values registers 
+  */
+  always@(posedge clk)
+    begin 
+      for(I=0;I<PORT_WIDTH;I=I+1)
+        if (chipselect && write && address==(I+16))
+          begin 
+            pwm_values[I] <= writedata;
+          end 
+    end 
+  
+  /* 
+     Generating PWM Pulses 
+     Only 1 counter is running, the rest are just comparator
+     PWM Rising edges of all pins are aligned
+  */
+  always@(posedge clk)
+    begin 
+      pwm_cntr <= pwm_cntr + 1;
+      for(I=0;I<PORT_WIDTH;I=I+1)
+      if (pwm_cntr<pwm_values[I]) 
+        pwm_out[I] <= pwm_ena[I]; 
+      else
+        pwm_out[I] <= 1'b0;
+    end 
+  
+  genvar IO;
+  generate 
+  for(IO=0;IO<PORT_WIDTH;IO=IO+1) 
+  begin: assignpins
+    assign io_oe[IO]   = pin_mode[IO]|pwm_ena[IO];
+    assign io_out[IO]  = pwm_ena[IO]?pwm_out[IO]:data_out[IO];
+    assign io_opdrn[IO]= pin_opdrn[IO];
+  end 
+  endgenerate 
+  
+
+endmodule
+
+
---
+++ hw/cmn/timer/recon_timer.v
@@ -0,0 +1,160 @@
+/*
+  Project: RECON 2017
+  Author: Jeff Lieu <lieumychuong@gmail.com>
+  Description: Timer module to support delay function
+*/
+
+/////////////////////////////////////////////////////////////////////////////////
+// synthesis translate_off
+`timescale 1ns / 1ps
+// synthesis translate_on
+
+
+`define     MILLISEC_OFFSET  0
+`define     SECOND_OFFSET    1
+`define     IRQ_TIME_OFFSET  2
+`define     CTRL_OFFSET      3
+
+`define     IRQ_COUNTDOWN_ENA_BIT    0
+`define     IRQ_TIMER_CONTINUOUS_BIT 1
+`define     IRQ_TIMER_MATCH_ENA_BIT  2
+
+
+module recon_timer (
+                      //Avalon slave
+                       address,
+                       chipselect,
+                       clk,
+                       reset_n,
+                       write,
+                       read,
+                       writedata,                       
+                       readdata,                      
+                       
+                       //IRQ
+                       irq,
+                       
+                       //Timing Pulses
+                       microsec_tick,
+                       millisec_tick,
+                       second_tick
+);
+  parameter CLK_FREQ = 50000000;
+  parameter MILLISEC = CLK_FREQ/1000-1; 
+  parameter MICROSEC = CLK_FREQ/1000000-1; 
+    
+  output  [ 31: 0] readdata;
+  input   [  2: 0] address;
+  input            chipselect;
+  input            clk;
+  input            reset_n;
+  input            write;
+  input            read;
+  input   [ 31: 0] writedata;
+  output           irq;
+  output  reg      microsec_tick;
+  output  reg      millisec_tick;
+  output  reg      second_tick;
+  wire    [ 31: 0] readdata;
+  
+  
+    
+  reg     [ 31: 0] read_mux_reg;
+  reg     [  9: 0] microsec;
+  reg     [ 31: 0] millisec;
+  reg     [ 31: 0] next_millisec;
+  reg     [ 31: 0] second;  
+  reg     [  3: 0] ctrl_reg;
+  reg     [ 31: 0] irq_timer;
+  reg     [ 31: 0] irq_time;
+  reg     [  9: 0] timer;
+  reg     irq_active;
+  reg     irq_active_d;
+  reg     [  3: 0] irq_pulse;
+  //s1, which is an e_avalon_slave
+
+  always@(posedge clk)
+  begin 
+    if (chipselect && read)
+      case(address)      
+      `MILLISEC_OFFSET : read_mux_reg <= millisec;
+      `SECOND_OFFSET   : read_mux_reg <= second;
+      `IRQ_TIME_OFFSET : read_mux_reg <= irq_time;
+      `CTRL_OFFSET     : read_mux_reg <= {28'h0, ctrl_reg};      
+      default          : read_mux_reg <= 32'h0;
+      endcase 
+    end 
+  assign readdata = read_mux_reg;
+  
+  always@(posedge clk or negedge reset_n)
+    if (~reset_n) begin 
+      millisec <= 0;
+      second   <= 0;            
+      next_millisec <= 999;
+      microsec_tick <= 1'b0;
+      millisec_tick <= 1'b0;
+      second_tick   <= 1'b0;
+    end else begin 
+      
+      /* Timer */
+      timer <= timer + 1;
+      millisec_tick <= 1'b0;
+      second_tick   <= 1'b0;
+      microsec_tick <= 1'b0;
+      if (timer == MICROSEC) begin       
+        timer    <= 0;
+        microsec <= microsec + 1;
+        microsec_tick <= 1'b1;
+        /* Increment millisec */
+        if (microsec == 999) begin 
+          microsec <= 0;
+          millisec <= millisec + 1;
+          millisec_tick <= 1'b1;
+          /* Increment second */
+          if (millisec == next_millisec) begin 
+              second   <= second+1;      
+              next_millisec <= next_millisec + 1000;
+              second_tick <= 1'b1;
+            end 
+          end
+      end 
+      
+      /* CTRL Register */
+      if (chipselect && write && address==`CTRL_OFFSET)
+        ctrl_reg     <= writedata;
+      
+      /* Timing Interval */
+      if (chipselect && write && address==`IRQ_TIME_OFFSET)
+        irq_time <= writedata;
+      
+      irq_active_d <= irq_active;
+      if (irq_active == 1'b1 && irq_active_d == 1'b0)
+        irq_pulse <= 4'b1111;
+      else 
+        irq_pulse <= {irq_pulse[2:0],1'b0};
+        
+      /* Interval Interrupt mode */
+      if (ctrl_reg[`IRQ_COUNTDOWN_ENA_BIT]==1'b1) begin 
+        irq_timer <= irq_timer + 1;
+        if (irq_timer == irq_time) begin 
+          irq_active <= 1'b1;
+          /* If Continuous Mode is enabled, we restart the timer */
+          if (ctrl_reg[`IRQ_TIMER_CONTINUOUS_BIT]==1'b1) begin 
+            irq_timer <= 0;
+          end           
+        end         
+      end else 
+      /* Match Mode Interrupt */
+        if (ctrl_reg[`IRQ_TIMER_MATCH_ENA_BIT]==1'b1) begin         
+          if (millisec == irq_time) begin 
+            irq_active <= 1'b1;          
+        end         
+      end
+        
+    end    
+    
+    assign irq = irq_pulse[3]; 
+    
+endmodule
+
+
