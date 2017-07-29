a925d7ef4e9a25c80bf2f6a5e7a61116b4ffbfc9
jefflieu <lieumychuong@gmail.com>
2017-07-29 14:20:08 +1000

    Update to Quartus17 and change method of generating programming files

---
+++ hw/recon_1/civ10/output_files/program_fpga_image_to_flash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+nios2-flash-programmer recon_1.flash --epcs --base=0x02000000
--- hw/recon_1/civ10/release/program_fpga_image_to_flash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-sof2flash --input=recon_1.sof --output=recon_1.flash --epcs --programmingmode=as
-nios2-flash-programmer recon_1.flash --epcs --base=0x02000000
